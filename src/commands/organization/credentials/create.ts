00be8e57db02f1bfa42fecf433c664b670735870
Taylor Reece <taylor.reece@prismatic.io>
2022-12-30 15:37:45 -0600

    Revert "Merge pull request #9 from prismatic-io/tr/remove-credentials"
    This reverts commit 4cdeb47ade556224e22dbc22e4609dc70766adbf, reversing
    changes made to ffdd839b67d53c7e35fa7b595f382629f4ea54ee.

---
+++ src/commands/organization/credentials/create.ts
@@ -0,0 +1,66 @@
+import { Command, Flags } from "@oclif/core";
+import { toValues } from "../../../fields";
+import { gql, gqlRequest } from "../../../graphql";
+
+export default class CreateCommand extends Command {
+  static description =
+    "Create a set of Organization-level Credentials for use by Instance Actions";
+
+  static flags = {
+    label: Flags.string({
+      char: "l",
+      required: true,
+      description: "name to give the new credentials",
+    }),
+    "authorization-method": Flags.string({
+      char: "a",
+      required: true,
+      description: "ID of the authorization method",
+    }),
+    fields: Flags.string({
+      char: "f",
+      description: "username, password, etc., in JSON format",
+    }),
+  };
+
+  async run() {
+    const {
+      flags: { label, "authorization-method": authorizationMethod, fields },
+    } = await this.parse(CreateCommand);
+
+    const values = toValues(fields);
+
+    const result = await gqlRequest({
+      document: gql`
+        mutation createOrganizationCredential(
+          $label: String!
+          $authorizationMethod: ID!
+          $values: [InputCredentialFieldValue]
+        ) {
+          createOrganizationCredential(
+            input: {
+              label: $label
+              authorizationMethod: $authorizationMethod
+              values: $values
+            }
+          ) {
+            errors {
+              field
+              messages
+            }
+            credential {
+              id
+            }
+          }
+        }
+      `,
+      variables: {
+        label,
+        authorizationMethod,
+        values,
+      },
+    });
+
+    this.log(result.createOrganizationCredential.credential.id);
+  }
+}
