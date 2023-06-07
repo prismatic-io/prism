f756c98950207c8dd2960a708e288b209c958c91
Taylor Reece <taylor.reece@prismatic.io>
2023-06-07 09:24:35 -0500

    Add filters to integraitons:list to only show specific customer or org-only integrations

--- src/commands/integrations/list.ts
+++ src/commands/integrations/list.ts
@@ -10,11 +10,20 @@ export default class ListCommand extends Command {
       description:
         "If specified this command returns all versions of all integrations rather than only the latest version",
     }),
+    customer: Flags.string({
+      char: "c",
+      description:
+        "If specified this command returns only integrations that are available to the specified customer ID",
+    }),
+    "org-only": Flags.boolean({
+      char: "o",
+      description: "If specified this command returns only org integrations",
+    }),
   };
 
   async run() {
     const { flags } = await this.parse(ListCommand);
-    const { showAllVersions } = flags;
+    const { showAllVersions, customer, "org-only": orgOnly } = flags;
 
     let integrations: any[] = [];
     let hasNextPage = true;
@@ -25,8 +34,18 @@ export default class ListCommand extends Command {
         integrations: { nodes, pageInfo },
       } = await gqlRequest({
         document: gql`
-          query listIntegrations($showAllVersions: Boolean, $after: String) {
-            integrations(allVersions: $showAllVersions, after: $after) {
+          query listIntegrations(
+            $showAllVersions: Boolean
+            $after: String
+            $customer: ID
+            $customerIsnull: Boolean
+          ) {
+            integrations(
+              allVersions: $showAllVersions
+              after: $after
+              customer: $customer
+              customer_Isnull: $customerIsnull
+            ) {
               nodes {
                 id
                 name
@@ -34,6 +53,11 @@ export default class ListCommand extends Command {
                 versionNumber
                 labels
                 category
+                customer {
+                  id
+                  name
+                  externalId
+                }
               }
               pageInfo {
                 hasNextPage
@@ -45,6 +69,8 @@ export default class ListCommand extends Command {
         variables: {
           showAllVersions,
           after: cursor,
+          customer,
+          customerIsnull: orgOnly,
         },
       });
       integrations = [...integrations, ...nodes];
@@ -64,6 +90,15 @@ export default class ListCommand extends Command {
         versionNumber: { header: "Version" },
         labels: { extended: true },
         category: { extended: true },
+        customerId: { extended: true, get: (row) => row.customer?.id ?? "" },
+        customerName: {
+          extended: true,
+          get: (row) => row.customer?.name ?? "",
+        },
+        customerExternalId: {
+          extended: true,
+          get: (row) => row.customer?.externalId ?? "",
+        },
       },
       { ...flags }
     );
