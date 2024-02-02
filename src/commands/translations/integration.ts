461ad6239ae81394473e966af841382d3ed65e52
awburgard <austin.burgard@prismatic.io>
2024-02-02 10:10:07 -0600

    adds output file for integration

--- src/commands/translations/integration.ts
+++ src/commands/translations/integration.ts
@@ -1,11 +1,21 @@
-import { Command, Args } from "@oclif/core";
+import { Command, Args, Flags } from "@oclif/core";
 import { gqlRequest } from "../../graphql";
 import { MarketplaceTranslations } from "../../types";
 import { processIntegrationsForTranslations } from "../../utils/translations/processDataForTranslations";
 import { GET_MARKETPLACE_INTEGRATIONS_TRANSLATIONS } from "../../queries.graphql";
+import { fs } from "../../fs";
 
 export default class TranslationsCommand extends Command {
   static description = "Generate Dynamic Phrases for a Particular Integration";
+
+  static flags = {
+    "output-file": Flags.string({
+      required: false,
+      char: "o",
+      description: "Output the results of the action to a specified file",
+      default: "translations_output.json",
+    }),
+  };
 
   static args = {
     integration: Args.string({
@@ -17,17 +27,24 @@ export default class TranslationsCommand extends Command {
   async run() {
     const {
       args: { integration },
+      flags: { "output-file": output },
     } = await this.parse(TranslationsCommand);
+
+    const cwd = process.cwd();
 
     const result = await gqlRequest<MarketplaceTranslations>({
       document: GET_MARKETPLACE_INTEGRATIONS_TRANSLATIONS,
-      variables: {
-        id: integration,
-      },
+      variables: { id: integration },
     });
 
-    const processedIntegations = processIntegrationsForTranslations(result);
+    const processedIntegrations = processIntegrationsForTranslations(result);
 
-    this.logJson(processedIntegations);
+    if (output) {
+      process.chdir(cwd);
+      this.log(`Writing translations to ${output}`);
+      fs.writeFile(output, JSON.stringify(processedIntegrations, null, 2));
+    } else {
+      this.logJson(processedIntegrations);
+    }
   }
 }
