448afe6d5f054e6122570ec509daa58e7ba41bb9
awburgard <austin.burgard@prismatic.io>
2024-02-06 08:48:25 -0600

    removes intergration level cmd

--- src/types.ts
+++ src/types.ts
@@ -526,3 +526,291 @@ export type EndpointSecurityTypeEnum =
   | "customer_optional"
   | "customer_required"
   | "organization";
+
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL fragment: IntegrationTranslation
+// ====================================================
+
+export interface IntegrationTranslation_requiredConfigVariables_nodes_inputs_nodes {
+  __typename: "Expression";
+  /**
+   * The name of the Expression.
+   */
+  name: string;
+  /**
+   * An object that contains arbitrary meta data about an Expression.
+   */
+  meta: Scalar_JSONString | null;
+}
+
+export interface IntegrationTranslation_requiredConfigVariables_nodes_inputs {
+  __typename: "ExpressionConnection";
+  /**
+   * List of nodes in this connection.
+   */
+  nodes: (IntegrationTranslation_requiredConfigVariables_nodes_inputs_nodes | null)[];
+}
+
+export interface IntegrationTranslation_requiredConfigVariables_nodes {
+  __typename: "RequiredConfigVariable";
+  /**
+   * The Key for the Required Config Variable. Referred to as 'Name' in the UI.
+   */
+  key: string;
+  /**
+   * Additional notes about the Required Config Var.
+   */
+  description: string | null;
+  /**
+   * The collection of Expressions that serve as inputs to the RequiredConfigVariable.
+   */
+  inputs: IntegrationTranslation_requiredConfigVariables_nodes_inputs | null;
+}
+
+export interface IntegrationTranslation_requiredConfigVariables {
+  __typename: "RequiredConfigVariableConnection";
+  /**
+   * List of nodes in this connection.
+   */
+  nodes: (IntegrationTranslation_requiredConfigVariables_nodes | null)[];
+}
+
+export interface IntegrationTranslation {
+  __typename: "Integration";
+  /**
+   * The name of the Integration.
+   */
+  name: string;
+  /**
+   * Additional notes about the Integration.
+   */
+  description: string | null;
+  /**
+   * The YAML that is the declarative definition for the Integration. Suitable for using to re-import the Integration.
+   */
+  definition: string | null;
+  /**
+   * Specifies the category of the Integration.
+   */
+  category: string | null;
+  /**
+   * Specifies an Overview of the Integration to describe its functionality for use in the Integration Marketplace.
+   */
+  overview: string | null;
+  /**
+   * A JSON string that represents deployment configuration pages.
+   */
+  configPages: Scalar_JSONString;
+  requiredConfigVariables: IntegrationTranslation_requiredConfigVariables;
+}
+
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: MarketplaceTranslation
+// ====================================================
+
+export interface MarketplaceTranslation_integration_requiredConfigVariables_nodes_inputs_nodes {
+  __typename: "Expression";
+  /**
+   * The name of the Expression.
+   */
+  name: string;
+  /**
+   * An object that contains arbitrary meta data about an Expression.
+   */
+  meta: Scalar_JSONString | null;
+}
+
+export interface MarketplaceTranslation_integration_requiredConfigVariables_nodes_inputs {
+  __typename: "ExpressionConnection";
+  /**
+   * List of nodes in this connection.
+   */
+  nodes: (MarketplaceTranslation_integration_requiredConfigVariables_nodes_inputs_nodes | null)[];
+}
+
+export interface MarketplaceTranslation_integration_requiredConfigVariables_nodes {
+  __typename: "RequiredConfigVariable";
+  /**
+   * The Key for the Required Config Variable. Referred to as 'Name' in the UI.
+   */
+  key: string;
+  /**
+   * Additional notes about the Required Config Var.
+   */
+  description: string | null;
+  /**
+   * The collection of Expressions that serve as inputs to the RequiredConfigVariable.
+   */
+  inputs: MarketplaceTranslation_integration_requiredConfigVariables_nodes_inputs | null;
+}
+
+export interface MarketplaceTranslation_integration_requiredConfigVariables {
+  __typename: "RequiredConfigVariableConnection";
+  /**
+   * List of nodes in this connection.
+   */
+  nodes: (MarketplaceTranslation_integration_requiredConfigVariables_nodes | null)[];
+}
+
+export interface MarketplaceTranslation_integration_instances_nodes_integration_requiredConfigVariables_nodes_inputs_nodes {
+  __typename: "Expression";
+  /**
+   * The name of the Expression.
+   */
+  name: string;
+  /**
+   * An object that contains arbitrary meta data about an Expression.
+   */
+  meta: Scalar_JSONString | null;
+}
+
+export interface MarketplaceTranslation_integration_instances_nodes_integration_requiredConfigVariables_nodes_inputs {
+  __typename: "ExpressionConnection";
+  /**
+   * List of nodes in this connection.
+   */
+  nodes: (MarketplaceTranslation_integration_instances_nodes_integration_requiredConfigVariables_nodes_inputs_nodes | null)[];
+}
+
+export interface MarketplaceTranslation_integration_instances_nodes_integration_requiredConfigVariables_nodes {
+  __typename: "RequiredConfigVariable";
+  /**
+   * The Key for the Required Config Variable. Referred to as 'Name' in the UI.
+   */
+  key: string;
+  /**
+   * Additional notes about the Required Config Var.
+   */
+  description: string | null;
+  /**
+   * The collection of Expressions that serve as inputs to the RequiredConfigVariable.
+   */
+  inputs: MarketplaceTranslation_integration_instances_nodes_integration_requiredConfigVariables_nodes_inputs | null;
+}
+
+export interface MarketplaceTranslation_integration_instances_nodes_integration_requiredConfigVariables {
+  __typename: "RequiredConfigVariableConnection";
+  /**
+   * List of nodes in this connection.
+   */
+  nodes: (MarketplaceTranslation_integration_instances_nodes_integration_requiredConfigVariables_nodes | null)[];
+}
+
+export interface MarketplaceTranslation_integration_instances_nodes_integration {
+  __typename: "Integration";
+  /**
+   * The name of the Integration.
+   */
+  name: string;
+  /**
+   * Additional notes about the Integration.
+   */
+  description: string | null;
+  /**
+   * The YAML that is the declarative definition for the Integration. Suitable for using to re-import the Integration.
+   */
+  definition: string | null;
+  /**
+   * Specifies the category of the Integration.
+   */
+  category: string | null;
+  /**
+   * Specifies an Overview of the Integration to describe its functionality for use in the Integration Marketplace.
+   */
+  overview: string | null;
+  /**
+   * A JSON string that represents deployment configuration pages.
+   */
+  configPages: Scalar_JSONString;
+  requiredConfigVariables: MarketplaceTranslation_integration_instances_nodes_integration_requiredConfigVariables;
+}
+
+export interface MarketplaceTranslation_integration_instances_nodes {
+  __typename: "Instance";
+  /**
+   * The ID of the object
+   */
+  id: string;
+  /**
+   * The name of the Instance.
+   */
+  name: string;
+  /**
+   * The Integration that has been deployed for the Instance.
+   */
+  integration: MarketplaceTranslation_integration_instances_nodes_integration;
+}
+
+export interface MarketplaceTranslation_integration_instances {
+  __typename: "InstanceConnection";
+  /**
+   * List of nodes in this connection.
+   */
+  nodes: (MarketplaceTranslation_integration_instances_nodes | null)[];
+}
+
+export interface MarketplaceTranslation_integration {
+  __typename: "Integration";
+  /**
+   * The ID of the object
+   */
+  id: string;
+  /**
+   * The name of the Integration.
+   */
+  name: string;
+  /**
+   * Additional notes about the Integration.
+   */
+  description: string | null;
+  /**
+   * The YAML that is the declarative definition for the Integration. Suitable for using to re-import the Integration.
+   */
+  definition: string | null;
+  /**
+   * Specifies the category of the Integration.
+   */
+  category: string | null;
+  /**
+   * Specifies an Overview of the Integration to describe its functionality for use in the Integration Marketplace.
+   */
+  overview: string | null;
+  /**
+   * A JSON string that represents deployment configuration pages.
+   */
+  configPages: Scalar_JSONString;
+  requiredConfigVariables: MarketplaceTranslation_integration_requiredConfigVariables;
+  /**
+   * The Integration that has been deployed for the Instance.
+   */
+  instances: MarketplaceTranslation_integration_instances;
+}
+
+export interface MarketplaceTranslation {
+  /**
+   *     Returns the specified Integration object.
+   *
+   * Access is permitted when any of the following condition(s) are met:
+   *     1. The signed-in User has any of the following permissions for the associated Organization: [org_admin_integration_permissions, org_manage_integrations, org_view_integrations].
+   *     2. The signed-in User has any of the following permissions for any version of the object: [integration_admin_permissions, integration_view, integration_edit, integration_remove].
+   *     3. The signed-in User has any of the following permissions for the associated Customer of Integrations available in the Marketplace: [customer_admin_instance_deploy, customer_manage_marketplace_integrations].
+   *     4. The signed-in User has any of the following permissions for the associated Customer of Integrations available in the Marketplace: [customer_access_marketplace_integrations].
+   *     5. The signed-in User has any of the following permissions for the object's 'customer' attribute: [customer_admin_integration_permissions, customer_manage_integrations, customer_view_integrations].
+   *     6. The Customer User has any of the following permissions for the Customer and the Objects Attribute template_configuration is AVAILABLE: [customer_admin_integration_permissions, customer_manage_integrations, customer_view_integrations].
+   */
+  integration: MarketplaceTranslation_integration | null;
+}
+
+export interface MarketplaceTranslationVariables {
+  id: string;
+}
