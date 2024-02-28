e326f7856d710748d3603072894e1320db4077a7
Ryan Wersal <ryan.wersal@prismatic.io>
2024-02-28 09:25:01 -0700

    Add on-prem resource delete command

---
+++ src/commands/on-prem-resources/delete.ts
@@ -0,0 +1,37 @@
+import { Command, Args } from "@oclif/core";
+import { gqlRequest, gql } from "../../graphql";
+
+export default class DeleteCommand extends Command {
+  static description = "Delete an On-Premise Resource";
+  static args = {
+    resource: Args.string({
+      required: true,
+      description: "ID of the On-Premise Resource to delete",
+    }),
+  };
+
+  async run() {
+    const {
+      args: { resource },
+    } = await this.parse(DeleteCommand);
+
+    await gqlRequest({
+      document: gql`
+        mutation deleteOnPremiseResource($id: ID!) {
+          deleteOnPremiseResource(input: { id: $id }) {
+            onPremiseResource {
+              id
+            }
+            errors {
+              field
+              messages
+            }
+          }
+        }
+      `,
+      variables: {
+        id: resource,
+      },
+    });
+  }
+}
