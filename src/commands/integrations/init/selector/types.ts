f16f735cae0d6df7698989ceb89cb0753f9e6a08
Ryan Wersal <ryan@ryanwersal.com>
2024-05-16 10:17:32 -0700

    Scaffold out private component selectors (#80)

--- src/commands/integrations/init/selector/types.ts
+++ src/commands/integrations/init/selector/types.ts
@@ -3,15 +3,13 @@ import { ChoiceOrSeparatorArray, Separator } from "inquirer-autocomplete-standal
 export const selectorTypes = ["trigger", "connection", "dataSource"] as const;
 export type SelectorTypes = (typeof selectorTypes)[number];
 
-export type ComponentKey = { key: string; id: string };
-export type SelectorKey = ComponentKey & {
+type BaseKey = { key: string; id: string };
+export type ComponentKey = BaseKey & { isPublic: boolean };
+export type SelectorKey = BaseKey & {
   inputs: { key: string; comments: string; valueType: string; required: boolean }[];
 };
 
-type Choice<TValue extends ComponentKey> = Exclude<
-  ChoiceOrSeparatorArray<TValue>[number],
-  Separator
->;
+type Choice<TValue extends BaseKey> = Exclude<ChoiceOrSeparatorArray<TValue>[number], Separator>;
 
 export interface SelectorStrategy {
   searchComponents: (input: string) => Promise<Choice<ComponentKey>[]>;
