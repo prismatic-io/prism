6ae9245134065afa693b33e3400c4ac579863074
Michael Halka <michael.halka@prismatic.io>
2024-05-29 10:09:24 -0700

    init commit

---
+++ src/commands/organization/connection/create.ts
@@ -0,0 +1,88 @@
+import { Command, Flags } from "@oclif/core";
+import { gqlRequest, gql } from "../../graphql.js";
+import { parseJsonOrUndefined } from "../../fields.js";
+
+export default class CreateCommand extends Command {
+  static description = "Create an Instance";
+  static flags = {
+    name: Flags.string({
+      char: "n",
+      required: true,
+      description: "name of your new instance.",
+    }),
+    integration: Flags.string({
+      char: "i",
+      required: true,
+      description:
+        "ID of the integration or a specific integration version ID this is an instance of",
+    }),
+    customer: Flags.string({
+      char: "c",
+      required: true,
+      description: "ID of customer to deploy to",
+    }),
+    description: Flags.string({
+      required: false,
+      char: "d",
+      description: "longer description of the instance",
+    }),
+    "config-vars": Flags.string({
+      required: false,
+      char: "v",
+      description: "config variables to bind to steps of your instance",
+    }),
+    label: Flags.string({
+      char: "l",
+      description: "a label or set of labels to apply to the instance",
+      multiple: true,
+    }),
+  };
+
+  async run() {
+    const {
+      flags: { name, description, integration, customer, "config-vars": configVars, label },
+    } = await this.parse(CreateCommand);
+
+    const result = await gqlRequest({
+      document: gql`
+        mutation createInstance(
+          $name: String!
+          $description: String
+          $integration: ID!
+          $customer: ID!
+          $configVariables: [InputInstanceConfigVariable]
+          $labels: [String]
+        ) {
+          createInstance(
+            input: {
+              name: $name
+              description: $description
+              integration: $integration
+              customer: $customer
+              configVariables: $configVariables
+              labels: $labels
+            }
+          ) {
+            instance {
+              id
+            }
+            errors {
+              field
+              messages
+            }
+          }
+        }
+      `,
+      variables: {
+        name,
+        description,
+        integration,
+        customer,
+        configVariables: parseJsonOrUndefined(configVars),
+        labels: label,
+      },
+    });
+
+    this.log(result.createInstance.instance.id);
+  }
+}
