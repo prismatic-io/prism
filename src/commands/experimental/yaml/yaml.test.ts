c4f4312781b7b375f2027367ff75c53fbb234208
Pattra Audcharevorakul <pattra.audcharevorakul@prismatic.io>
2025-03-24 11:04:01 -0400

    Re-add

--- src/commands/experimental/yaml/yaml.test.ts
+++ src/commands/experimental/yaml/yaml.test.ts
@@ -5,54 +5,54 @@ import path from "path";
 import { readFile } from "fs-extra";
 import { walkDir } from "../../../fs.js";
 
-const FLOW_GENERATION_TIMEOUT = 5 * 60 * 1; // 1 minute
+const CONVERT_GENERATION_TIMEOUT = 5 * 60 * 5; // 5 minutes
 interface SpecMeta {
   fileName: string;
   name: string;
 }
 
-// describe("YAML CNI generation tests", () => {
-//   const basePath = process.cwd();
-//   const commandPath = path.resolve("src/commands/experimental/yaml");
-//   const tempPath = path.resolve(`${commandPath}/temp`);
+describe("YAML CNI generation tests", () => {
+  const basePath = process.cwd();
+  const commandPath = path.resolve("src/commands/experimental/yaml");
+  const tempPath = path.resolve(`${commandPath}/temp`);
 
-//   if (!fs.existsSync(tempPath)) {
-//     fs.mkdirSync(tempPath);
-//   }
+  if (!fs.existsSync(tempPath)) {
+    fs.mkdirSync(tempPath);
+  }
 
-//   const specs = fs.readdirSync(`${commandPath}/fixtures/specs`).map<SpecMeta>((fileName) => {
-//     const [name] = fileName.toLowerCase().split(".");
-//     return { fileName, name };
-//   });
+  const specs = fs.readdirSync(`${commandPath}/fixtures/specs`).map<SpecMeta>((fileName) => {
+    const [name] = fileName.toLowerCase().split(".");
+    return { fileName, name };
+  });
 
-//   for (const { fileName, name } of specs) {
-//     describe(name, () => {
-//       it(
-//         "should generate successfully",
-//         async () => {
-//           process.chdir(tempPath);
+  for (const { fileName, name } of specs) {
+    describe(name, () => {
+      it(
+        "should generate successfully",
+        async () => {
+          process.chdir(tempPath);
 
-//           await GenerateIntegrationFromYAMLCommand.run([
-//             `--yamlFile=../fixtures/specs/${fileName}`,
-//           ]);
+          await GenerateIntegrationFromYAMLCommand.run([
+            `--yamlFile=../fixtures/specs/${fileName}`,
+          ]);
 
-//           expect(process.cwd()).toStrictEqual(tempPath);
+          expect(process.cwd()).toStrictEqual(tempPath);
 
-//           process.chdir(basePath);
-//         },
-//         FLOW_GENERATION_TIMEOUT,
-//       );
+          process.chdir(basePath);
+        },
+        CONVERT_GENERATION_TIMEOUT,
+      );
 
-//       it("should match scaffolding snapshots", async () => {
-//         process.chdir(tempPath);
-//         const targets = await walkDir(name, [".png", "webpack.config.js", "package.json"]);
-//         for (const target of targets) {
-//           const contents = await readFile(target, "utf-8");
-//           expect(contents).toMatchSnapshot(target);
-//         }
+      // it("should match scaffolding snapshots", async () => {
+      //   process.chdir(tempPath);
+      //   const targets = await walkDir(name, [".png", "webpack.config.js", "package.json"]);
+      //   for (const target of targets) {
+      //     const contents = await readFile(target, "utf-8");
+      //     expect(contents).toMatchSnapshot(target);
+      //   }
 
-//         process.chdir(basePath);
-//       });
-//     });
-//   }
-// });
+      //   process.chdir(basePath);
+      // });
+    });
+  }
+});
