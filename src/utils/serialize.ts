e29dadb3e7a303674b425c9150547c5ccb8df0dd
Taylor Reece <taylor.reece@prismatic.io>
2025-04-23 15:47:42 -0500

    integrations:export should export double-quoted YAML definitions

--- src/utils/serialize.ts
+++ src/utils/serialize.ts
@@ -3,7 +3,7 @@ import { dump, load, DumpOptions, LoadOptions } from "js-yaml";
 export const dumpYaml = (
   value: unknown,
   extraOptions?: Partial<DumpOptions>,
-): ReturnType<typeof dump> => dump(value, { ...extraOptions, noRefs: true });
+): ReturnType<typeof dump> => dump(value, { ...extraOptions, noRefs: true, quotingType: '"' });
 
 export const loadYaml = <T = unknown>(value: string, extraOptions?: Partial<LoadOptions>): T =>
   load(value, { ...extraOptions }) as T;
