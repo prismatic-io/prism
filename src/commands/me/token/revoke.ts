76b9b9fb6ec6767f5745045ed9a8db5c66d9bec0
Ryan Wersal <ryan.wersal@prismatic.io>
2026-03-02 13:42:02 -0700

    Add confirmation when revoking tokens

--- src/commands/me/token/revoke.ts
+++ src/commands/me/token/revoke.ts
@@ -1,10 +1,32 @@
-import { Command } from "@oclif/core";
+import { Flags, ux } from "@oclif/core";
 import { revokeRefreshToken } from "../../../auth.js";
+import { PrismaticBaseCommand } from "../../../baseCommand.js";
 
-export default class RevokeTokenCommand extends Command {
+export default class RevokeTokenCommand extends PrismaticBaseCommand {
   static description = "Revoke all refresh tokens for your user";
 
+  static flags = {
+    confirm: Flags.boolean({
+      allowNo: true,
+      default: true,
+      description: "Prompt for confirmation before revoking tokens. Use --no-confirm to skip.",
+    }),
+  };
+
   async run() {
+    const {
+      flags: { confirm },
+    } = await this.parse(RevokeTokenCommand);
+
+    if (confirm) {
+      const shouldContinue = await ux.confirm(
+        "This will revoke all refresh tokens for the current user. Continue? (yes/no)",
+      );
+      if (!shouldContinue) {
+        this.error("Operation canceled", { exit: 1 });
+      }
+    }
+
     await revokeRefreshToken();
     this.log("All refresh tokens for your user have been revoked.");
   }
